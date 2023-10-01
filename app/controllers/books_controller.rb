31d01feb390aa4a77f25e1830bda6aa16a5734b3
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-01 16:09:24 +0000

    [Add]book-index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,13 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book)
   end
 
   def index
     @books = Book.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
@@ -21,6 +23,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
