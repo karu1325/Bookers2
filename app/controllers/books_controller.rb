9e961b8baf281ff28bfb4771b77c802def336d14
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-02 17:19:51 +0000

    [Add]show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+    @book_new = Book.new
   end
 
   def create
@@ -13,11 +13,23 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @user = current_user
-    @book = Book.new
+    @book_new = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
