78b090f7a55348afb7f62e56ac2abc3f7261f2d4
karu1325 <143956677+karu1325@users.noreply.github.com>
2023-10-08 15:33:23 +0000

    [Finish]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = current_user
+    @book_new = Book.new
   end
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
